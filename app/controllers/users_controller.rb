fd758b4935bd7f5f1af3d7f1a4b699f156556a71
Akintola Ramota <akintolarahmah@gmail.com>
2020-10-13 12:28:45 +0000

    Add controller set-ups for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  before_action :set_user, except: %i[new create]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @users = User.find(set_user)
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Object successfully created"
+      redirect_to @user
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(user_params)
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.fetch(:user).permit(:name)
+  end
 end
