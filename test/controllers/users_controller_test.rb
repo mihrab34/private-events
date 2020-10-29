ff6df0e4bbcfa47155ef3992ed65cbec6eb084d4
Akintola Ramota <akintolarahmah@gmail.com>
2020-10-29 17:04:41 +0000

    Fixed Active record error on Event model

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create user' do
     assert_difference('User.count') do
-      post users_url, params: { user: {} }
+      post users_url, params: { user: { name: @user.name } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -26,5 +26,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should show user' do
     get user_url(@user)
     assert_response :success
+  end
+
+  test 'should update user' do
+    patch user_url(@user), params: { user: { name: @user.name } }
+    assert_redirected_to user_url(@user)
+  end
+
+  test 'should destroy user' do
+    assert_difference('User.count', -1) do
+      delete user_url(@user)
+    end
+
+    assert_redirected_to users_url
   end
 end
