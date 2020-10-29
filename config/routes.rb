abb9b0ff2084a42a733b8bfd2f410d92280d5793
Akintola Ramota <akintolarahmah@gmail.com>
2020-10-29 14:15:53 +0000

    Added Attendees to event page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :sessions, only: %i[new create destroy]
-  resources :events
+  resources :events do
+    member do
+      post 'attend', to: 'invitations#create'
+      delete 'unattend', to: 'invitations#destroy'
+    end
+  end
+
   root 'events#index'
 
   get '/login', to: 'sessions#new'
